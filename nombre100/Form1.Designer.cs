6a56873f69bd389eee69854e030039556681a26e
Kevin Arias <kevinlh1337@gmail.com>
2019-09-14 12:52:01 -0500

    Se sube el proyecto a github/rama remota

---
+++ nombre100/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace nombre100
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabla = new System.Windows.Forms.DataGridView();
+            this.iniciar = new System.Windows.Forms.Button();
+            this.columna1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columna2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabla
+            // 
+            this.tabla.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columna1,
+            this.columna2});
+            this.tabla.Location = new System.Drawing.Point(232, 12);
+            this.tabla.Name = "tabla";
+            this.tabla.Size = new System.Drawing.Size(224, 183);
+            this.tabla.TabIndex = 0;
+            // 
+            // iniciar
+            // 
+            this.iniciar.Location = new System.Drawing.Point(462, 171);
+            this.iniciar.Name = "iniciar";
+            this.iniciar.Size = new System.Drawing.Size(75, 23);
+            this.iniciar.TabIndex = 1;
+            this.iniciar.Text = "Iniciar";
+            this.iniciar.UseVisualStyleBackColor = true;
+            this.iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // columna1
+            // 
+            this.columna1.HeaderText = "Nombre";
+            this.columna1.Name = "columna1";
+            // 
+            // columna2
+            // 
+            this.columna2.HeaderText = "Contador";
+            this.columna2.Name = "columna2";
+            this.columna2.Width = 80;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 207);
+            this.Controls.Add(this.iniciar);
+            this.Controls.Add(this.tabla);
+            this.Name = "Form1";
+            this.Text = "Nombre 100 veces.";
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tabla;
+        private System.Windows.Forms.Button iniciar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columna1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columna2;
+    }
+}
+
